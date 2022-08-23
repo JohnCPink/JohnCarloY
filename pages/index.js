37746ea7a5447a7e10994fc7419ff69137aae9a9
john carlo yambao <jcdyambao.student@ua.edu.ph>
2022-08-23 22:08:17 +0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>"John Carlo Yambao, from Minalin , my hobbies are playing games, walking my dog.
+        I like doing computer stuff that’s why i take up IT,I see myself working on a company and being a manager, And i want to learn a lot of valuable skills and knowledge "</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
